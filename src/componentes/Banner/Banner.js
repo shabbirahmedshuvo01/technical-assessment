2dae290d2f711a359188577789a08c8678bb5718
Shabbir Ahmed Shuvo <Shabbir Ahmed Shuvo>
2022-09-28 18:29:36 +0600

    gradiant button added

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -2,15 +2,19 @@ import React from 'react';
 import bimg from "../../componentes/Images/Landing page image.svg"
 
 const Banner = () => {
+
+    const fclr = '#8456EC';
+    const sclr = '#E87BF8';
+
     return (
         <div className='bg-pink-50'>
             <section className='lg:flex lg:flex-row-reverse items-center justify-evenly max-w-7xl mx-auto'>
 
                 <div className='lg:text-end lg:my-40 lg:p-28 mb-20'>
-                    <h2 className='text-4xl font-bold text-primary'> <span>Imagine if</span> <br /> <span className='text-secondary'>Snapchat</span> <br /> had events.</h2>
+                    <h2 className='text-4xl font-bold text-primary'> <span>Imagine if</span> <br /> <span className='text-purple-600'>Snapchat</span> <br /> had events.</h2>
                     <p className=' text-gray-600'>Easily host and share events with your <br /> friends  across any social media.</p>
                     <div>
-                        <a href="/event"><button className="btn btn-active btn-secondary mt-5">🎉 Create my event</button></a>
+                        <a href="/event"><button className="btn bg-gradient-to-r from-purple-600 to-purple-500 mt-5 text-white">🎉 Create my event</button></a>
                     </div>
                 </div>
 
