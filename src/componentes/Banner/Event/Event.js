64c65294281719bf9b28562648ea6d8574884cac
Shabbir Ahmed Shuvo <Shabbir Ahmed Shuvo>
2022-09-28 17:43:19 +0600

    event page works done

--- src/componentes/Banner/Event/Event.js
+++ src/componentes/Banner/Event/Event.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import bdyImg from '../../Images/Birthday cake.png'
+import bdyImg from '../../Images/Birthday cake.png';
+import svg1 from '../../Images/Fill 19.svg'
 
 const Event = () => {
     return (
         <div>
-            <section className='flex'>
-                <div>
-                    <h2>Birthday</h2>
+            <section className='lg:flex flex-row-reverse justify-center items-center lg:gap-x-40'>
+
+                <div className='lg:mt-44'>
+                    <img className='w-96' src={bdyImg} alt="" />
                 </div>
+
                 <div>
-                    <img src={bdyImg} alt="" />
+                    <div className='text-start'>
+                        <h2 className='text-4xl font-bold text-primary'>Birthday Bash</h2>
+                        <p className='text-xs mt-1 text-black font-medium'>Hosted by Elysia</p>
+                    </div>
+
+                    {/* svg or texts */}
+
+                    <div className='mt-10'>
+                        <div className='mt-5 flex gap-5 text-start'>
+                            <div className='mt-2'>
+                                <img src={svg1} alt="" />
+                            </div>
+                            <div>
+                                <p className='text-primary font-semibold'>18 August 6:00PM</p>
+                                <p>to <span className='text-black font-medium'>19 August 1:00PM</span> UTC +10</p>
+                            </div>
+                        </div>
+                        <div className='mt-5 flex gap-5 text-start'>
+                            <div className='mt-2'>
+                                <img src={svg1} alt="" />
+                            </div>
+                            <div>
+                                <p className='text-primary font-semibold'>Street name</p>
+                                <p>Suburb, State, Postcode</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </section>
         </div>
