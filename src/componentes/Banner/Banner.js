855f3cdab88214a11c43cc9e14ab83d7f8cddeec
Shabbir Ahmed Shuvo <Shabbir Ahmed Shuvo>
2022-09-28 16:50:40 +0600

    banner responsive

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -9,7 +9,9 @@ const Banner = () => {
                 <div className='lg:text-end lg:my-40 lg:p-28 mb-20'>
                     <h2 className='text-4xl font-bold text-primary'> <span>Imagine if</span> <br /> <span className='text-secondary'>Snapchat</span> <br /> had events.</h2>
                     <p className=' text-gray-600'>Easily host and share events with your friends <br /> across any social media.</p>
-                    <button className="btn btn-active btn-secondary mt-5">🎉 Create my event</button>
+                    <div>
+                        <button className="btn btn-active btn-secondary mt-5">🎉 Create my event</button>
+                    </div>
                 </div>
 
                 <div className='py-6'>
